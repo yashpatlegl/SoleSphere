83dc06c8a81d74ce6aa7d040d36b313843781956
AyushiChandel11 <ayushi.chandel@globallogic.com>
2025-04-30 00:23:24 +0530

    done and tested

--- navbar.js
+++ navbar.js
@@ -1,22 +1,18 @@
 
 function updateNavbar() {
-    let iconDiv = document.querySelector(".icon");
-    
-    // Check user login status
-    if (sessionStorage.getItem("userLoggedIn") === "true") {
-      // Create wishlist icon
+  let iconDiv = document.querySelector(".icon");
+
+  if (sessionStorage.getItem("userLoggedIn") === "true") {
       let wishlistIcon = document.createElement("i");
       wishlistIcon.className = "fa fa-heart";
       wishlistIcon.style.fontSize = "24px";
-  
-      // Create shopping bag icon
+
       let shoppingBagIcon = document.createElement("i");
       shoppingBagIcon.className = "fa fa-shopping-bag";
       shoppingBagIcon.style.fontSize = "24px";
-  
-      // Create logout button
+
       let logoutButton = document.createElement("button");
-      logoutButton.setAttribute("id", "ayushi");
+      logoutButton.setAttribute("id", "logout-button");
       logoutButton.textContent = "Logout";
       logoutButton.style.marginLeft = "10px";
       logoutButton.style.padding = "5px 10px";
@@ -24,23 +20,16 @@ function updateNavbar() {
       logoutButton.style.backgroundColor = "red";
       logoutButton.style.color = "white";
       logoutButton.style.cursor = "pointer";
-  
-      // Append user-related icons
+
       iconDiv.appendChild(wishlistIcon);
       iconDiv.appendChild(shoppingBagIcon);
       iconDiv.appendChild(logoutButton);
-  
-      // Add event listener to logout button
-      logoutButton.addEventListener("click", function() {
-        // Remove wishlist and shopping bag icons
-        if (wishlistIcon && shoppingBagIcon) {
-          iconDiv.removeChild(wishlistIcon);
-          iconDiv.removeChild(shoppingBagIcon);
-        }
+
+      logoutButton.addEventListener("click", function () {
+          sessionStorage.removeItem("userLoggedIn");
+          location.reload();
       });
-    }
   }
-  
-  // Initialize navbar on page load
-  window.onload = updateNavbar;
-  
+}
+
+window.onload = updateNavbar;
